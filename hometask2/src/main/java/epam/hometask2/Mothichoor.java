5d0877dc8e29ef761849ba216c8fcc969d374238
Shruthi-Ade <shruthiade19@gmail.com>
2020-02-07 18:59:38 +0530

    second repo

---
+++ hometask2/src/main/java/epam/hometask2/Mothichoor.java
@@ -0,0 +1,13 @@
+package epam.hometask2;
+
+public class Mothichoor extends Sweets {
+    
+    public  int calcwt(int quantity,int weight)
+    {   
+        return weight*quantity;
+     
+    }
+}
+
+
+
